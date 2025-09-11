316a357a1224907ba6477a4984c4ed7bb8f8e603
Joao Vitor de Lara <joao.lara@bomjesus.br>
2025-09-11 17:30:09 -0300

    create card

--- src/components/Cards/ProfileCard.tsx
+++ src/components/Cards/ProfileCard.tsx
@@ -1,27 +1,39 @@
 export default function ProfileCard() {
   return (
-    <div className="p-4 border border-primary rounded-lg bg-secondary text-center">
-      <img
-        src="/profile.jpg"
-        alt="Profile Picture"
-        className="w-24 h-24 rounded-full mx-auto mb-4"
-      />
-      <h2 className="text-xl font-semibold mb-2">João Vitor de Lara</h2>
-      <p className="text-gray-300 mb-4">Desenvolvedor Web e Front-End</p>
-      <div className="flex justify-center space-x-4">
-        <a
-          href="#"
-          className="text-blue-500 hover:underline"
-        >
-          LinkedIn
-        </a>
-        <a
-          href="#"
-          className="text-blue-500 hover:underline"
-        >
-          GitHub
-        </a>
+    <div className="card-dev">
+      <div>
+
+        <img src="images/image.jpg" alt="Profile" className="border-full"/>
+
+        <div>
+          <p>Nome</p>
+          <p>Profissão</p>
+        </div>
+
+        <div>
+          <ul> contato
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+            <li>Link 4</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Habilidade 1</li>
+            <li>Habilidade 2</li>
+            <li>Habilidade 3</li>
+            <li>Habilidade 4</li>
+          </ul>
+        </div>
+
+        <div>
+          <button>Download CV</button>
+        </div>
+
       </div>
     </div>
+
   )
 }   
