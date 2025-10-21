1f33bae5ef3b896f54ea6b929b0e4ea7f11799ea
Joao Vitor de Lara <joao.lara@bomjesus.br>
2025-10-21 14:03:51 -0300

    fix: hover

--- src/components/Sections/CalltoAction.tsx
+++ src/components/Sections/CalltoAction.tsx
@@ -6,29 +6,28 @@ import SequentialFadeUp from "@/components/Animation/SequentialFadeUp";
 
 export default function Contact() {
   const { title, description, contact, social, button } = CtaSection;
-  
+
   return (
-    <section id="contact" className="min-h-[70vh] flex flex-col items-center justify-center bg-gradient-to-t from-stone-950">
+    <section
+      id="contact"
+      className="min-h-[70vh] flex flex-col items-center justify-center bg-gradient-to-t from-stone-950 relative"
+    >
       <div className="container p-5 space-y-3">
         <SequentialFadeUp staggerDelay={0.4} duration={0.6}>
-          <h2 className="textcolor-primary mb-10">
-            {title}
-          </h2>
+          <h2 className="textcolor-primary mb-10">{title}</h2>
 
           <h3 className="leading-tight mb-16 text-neutral-200 text-4xl">
             {description}
           </h3>
 
-          <SequentialFadeUp 
-            className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16" 
-            staggerDelay={0.3} 
+          <SequentialFadeUp
+            className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16"
+            staggerDelay={0.3}
             duration={0.5}
           >
             {/* Coluna 1 - Contato */}
             <div>
-              <h4 className="uppercase tracking-wider mb-4"> 
-                {contact.title}
-              </h4>
+              <h4 className="uppercase tracking-wider mb-4">{contact.title}</h4>
               <div className="space-y-2">
                 <p>{contact.email}</p>
                 <p>{contact.phone}</p>
@@ -37,12 +36,19 @@ export default function Contact() {
 
             {/* Coluna 2 - Social */}
             <div>
-              <h4 className="uppercase tracking-wider mb-4">
-                {social.title}
-              </h4>
-              <div className="space-y-2">
-                <p>{social.platforms}</p>
-                <p>{social.additionalPlatforms}</p>
+              <h4 className="uppercase tracking-wider mb-4">{social.title}</h4>
+                <div className="flex gap-6 mt-4 md:mt-0">
+                {CtaSection.social.socialLinks.map((social) => (
+                  <a
+                    key={social.name}
+                    href={social.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label={social.name}
+                  >
+                    {social.name}
+                  </a>
+                ))}
               </div>
             </div>
 
