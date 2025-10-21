1cf8d94b6c4eb1b62d72964433500f74509e238d
Joao Vitor de Lara <joao.lara@bomjesus.br>
2025-10-21 13:31:22 -0300

    fix

--- src/components/Sections/CalltoAction.tsx
+++ src/components/Sections/CalltoAction.tsx
@@ -3,12 +3,14 @@ import { CtaSection } from "@/data/data.texts";
 
 export default function Contact() {
   const { title, description, contact, social, button } = CtaSection;
-  
+
   return (
-    <section id="contact" className="min-h-[70vh] flex flex-col items-center justify-center bg-gradient-to-t from-stone-950">
+    <section
+      id="contact"
+      className="min-h-[70vh] flex flex-col items-center justify-center bg-gradient-to-t from-stone-950"
+    >
       <div className="container p-5 space-y-3">
-          <h2 className="textcolor-primary mb-10">
-          {title}</h2>
+        <h2 className="textcolor-primary mb-10">{title}</h2>
 
         <div className="">
           <h3 className="leading-tight mb-16 text-neutral-200 text-4xl">
@@ -18,9 +20,7 @@ export default function Contact() {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16">
             {/* Coluna 1 - Contato */}
             <div>
-              <h4 className="uppercase tracking-wider mb-4"> 
-                {contact.title}
-              </h4>
+              <h4 className="uppercase tracking-wider mb-4">{contact.title}</h4>
               <div className="space-y-2">
                 <p>{contact.email}</p>
                 <p>{contact.phone}</p>
@@ -29,12 +29,21 @@ export default function Contact() {
 
             {/* Coluna 2 - Social */}
             <div>
-              <h4 className="uppercase tracking-wider mb-4">
-                {social.title}
-              </h4>
+              <h4 className="uppercase tracking-wider mb-4">{social.title}</h4>
               <div className="space-y-2">
-                <p>{social.platforms}</p>
-                <p>{social.additionalPlatforms}</p>
+                <div className="flex gap-6 mt-4 md:mt-0">
+                  {CtaSection.social.socialLinks.map((social) => (
+                    <a
+                      key={social.name}
+                      href={social.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label={social.name}
+                    >
+                      {social.name}
+                    </a>
+                  ))}
+                </div>
               </div>
             </div>
 
