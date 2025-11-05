e801f073a007683270a40efe4dea937ceb24f5ec
João Lara <117029722+joaovlara@users.noreply.github.com>
2025-11-05 10:31:27 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  assetPrefix: isProd ? "/joaovlara.github.io/" : "",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, 
 };
 
 export default nextConfig;
